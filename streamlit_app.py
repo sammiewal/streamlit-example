30819c14f24a41c9932876d91645a14ae9caf0c5
sammiewal <115966126+sammiewal@users.noreply.github.com>
2023-11-15 16:01:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,81 @@
+import os
+import requests
+import json
 import pandas as pd
-import re
-from bs4 import BeautifulSoup
-import nltk
+import nltk, re, pprint
+from urllib import request
+from bs4 import BeautifulSoup                                                                                   # needed for parsing HTML
+import contractions                                                                                             # contractions dictionary
+from string import punctuation
+import spacy                                                                                                    # used for lemmatization/stemming
+from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+tokenizer = ToktokTokenizer()                                                                                   # stopword removal
+from nltk import word_tokenize
+import pandas as pd
+import numpy as np                                                                                              # general packages for data manipulation
 
-# Load the dataset
-data = pd.read_csv("/content/training.1600000.processed.noemoticon.csv", encoding='ISO-8859-1', header=None)
-data.columns = ["sentiment", "id", "date", "flag", "user", "text"]
+# Your GitHub Personal Access Token
+TOKEN = st.secrets["github_api"]
 
-def clean_text(tweet):
-    # Decoding HTML
-    tweet = BeautifulSoup(tweet, "lxml").get_text()
-    # Convert to lowercase
-    tweet = tweet.lower()
-    # Remove URLs
-    tweet = re.sub(r'http\S+', '', tweet)
-    # Remove mentions
-    tweet = re.sub(r'@\S+', '', tweet)
-    # Remove hashtags (just the # symbol)
-    tweet = re.sub(r'#', '', tweet)
-    # Remove punctuations
-    tweet = re.sub(r'[^\w\s]', '', tweet)
+# Define the API endpoint
+endpoint = "https://api.github.com/search/repositories"
 
-    # Text of characters to exclude
-    exclude_text = "中英文敏感词语言检测中外手机电话归属地运营商查询名字推断性别手机号抽取身份证抽取邮箱抽取中日"
+# Keywords you want to search for
+keywords = ['api', 'tensorflow', 'machine learning', 'python', 'ajax' ,'cloud computing', 'twitter', 'chatgpt', 'pytorch', 'keras', 'ai', 'django', 'ruby', 'ohmyzsh', 'arduino', 'chrome', 'wordpress', 'vinta', 'development', 'twbs', 'bootstrap', 'cybersecurity', 'docker', 'public api', 'aws', 'ibm', 'bitcoin', 'microsoft', 'drupal', 'matplotlib', 'seaborn', 'freecodecamp', 'react', 'donnemartin', 'html5', 'android',  'typescript', 'kamranahmedse', 'azure', 'atom', 'mysql', 'database', 'algorithm', 'nextjs', 'sass', 'sindresorhus', 'css', 'config', 'nltk', 'spacy', 'java', 'nodejs', 'javascript', 'scikit learn', 'jwasham',  'ebookfoundation', 'jquery', 'angular']
 
-    # Check if the tweet contains any character from exclude_text
-    if any(char in tweet for char in exclude_text):
-        return ''
+# Set up the headers
+headers = {
+    "Authorization": f"token {TOKEN}"
+}
 
-    # Tokenization
-    tokens = nltk.word_tokenize(tweet)
+# Initialize an empty list to store DataFrames for each keyword
+dfs = []
 
-    # Remove stopwords (optional based on performance)
-    stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
+# Initialize an empty list to store the JSON response data
+json_responses = []
 
-    # Lemmatization (optional based on performance)
-    # lemmatizer = WordNetLemmatizer()
-    # tokens = [lemmatizer.lemmatize(word) for word in tokens]
+for keyword in keywords:
+    # Define the query parameters for the current keyword and language filter
+    params = {
+        "q": f"{keyword}",  # Keyword and language filter
+        "sort": "stars",  # Sort by stars (you can change this to other criteria)
+        "order": "desc"   # Order by descending (you can change to "asc" for ascending)
+    }
 
-    # Join tokens back into a string
-    cleaned_tweet = ' '.join(tokens)
+    # Send a GET request to the API endpoint
+    response = requests.get(endpoint, headers=headers, params=params)
 
-    return cleaned_tweet
-# Apply the cleaning function to the text column
-data["cleaned_text"] = data["text"].apply(clean_text)
+    # Check the response status code
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        repositories = data["items"]
+
+        # Create a DataFrame from the repository data
+        df = pd.DataFrame(repositories)
+
+        # Select and rename columns of interest
+        df = df[["name", "html_url", "description", "stargazers_count", "forks_count", "watchers_count"]]
+        df.columns = ["Repository Name", "Repository URL", "Description", "Stars", "Forks", "Watchers"]
+
+        # Add a new column for the keyword
+        df["Keyword"] = keyword
+
+        # Append the DataFrame to the list
+        dfs.append(df)
+
+        # Append the JSON response to the list
+        json_responses.append(data)
+    else:
+        print(f"Failed to retrieve repositories for '{keyword}'. Status code: {response.status_code}")
+
+# Combine DataFrames for each keyword into a single DataFrame
+combined_df = pd.concat(dfs, ignore_index=True)
+
+# Display the combined DataFrame
+print(combined_df)
+
+# Save the JSON responses to a file
+with open('response.json', 'w', encoding='utf-8') as json_file:
+    json.dump(json_responses, json_file, ensure_ascii=False, indent=4)
