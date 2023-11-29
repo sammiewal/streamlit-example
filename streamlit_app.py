0877a0e8d7c0ec9c641c480709636098f34f7e7c
sammiewal <115966126+sammiewal@users.noreply.github.com>
2023-11-29 15:55:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from main_page import app as main_page_app
-from text_exploration import * as text_exploration_app
+from text_exploration import *
 from recommend import app as recommend_app
 from topic_model import app as topic_model_app
 
 PAGES = {
     "Main Page": main_page_app,
-    "Text Exploration": text_exploration_app,
     "Repository Recommendation": recommend_app,
     "Topic Modeling": topic_model_app
 }
