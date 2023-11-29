b7efaad166ff06598e85ca7569c3765b273efdc4
sammiewal <115966126+sammiewal@users.noreply.github.com>
2023-11-29 15:56:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from main_page import app as main_page_app
-from text_exploration import *
 from recommend import app as recommend_app
 from topic_model import app as topic_model_app
 
