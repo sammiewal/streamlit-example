f91809c6be50d75ab31abb7bfb1d1159b4da3087
sammiewal <115966126+sammiewal@users.noreply.github.com>
2023-11-29 15:58:48 -0700

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,10 +1,9 @@
 # main_page.py
 
 import streamlit as st
+from text_exploration import *
 
 def app():
     st.title('Main Page')
     st.write('This is the main page of the app.')
-
-    from text_exploration import *
 
