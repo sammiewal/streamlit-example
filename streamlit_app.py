6124f573f39674cfb56f73dd858240e3bcdf735c
sammiewal <115966126+sammiewal@users.noreply.github.com>
2023-11-29 16:31:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,5 @@
 import streamlit as st
-from main_page import app as main_page_app
-from recommend import app as recommend_app
-from topic_model import app as topic_model_app
 
-PAGES = {
-    "Main Page": main_page_app,
-    "Repository Recommendation": recommend_app,
-    "Topic Modeling": topic_model_app
-}
-
-def main():
-    st.sidebar.title('Navigation')
-    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
-    page = PAGES[selection]
-    page()
-
-if __name__ == "__main__":
-    main()
 
 
 
