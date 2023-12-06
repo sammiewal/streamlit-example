c5455f3e2a00e94d57a607ac0fa26624096a6eb5
sammiewal <115966126+sammiewal@users.noreply.github.com>
2023-12-06 10:44:25 -0700

    Update text_exploration.py

--- text_exploration.py
+++ text_exploration.py
@@ -481,3 +481,69 @@ for cluster_num in filtered_clusters:
     #print('Exemplar:', exemplar_repository)
     #print('Popular Repositories:', repositories)
     #print('-'*80)
+
+def normalize_document(doc):
+    doc = re.sub(r'[^a-zA-Z0-9\s]', '', doc, re.I|re.A)                                       # lower case and remove special characters\whitespaces
+    doc = doc.lower()
+    doc = doc.strip()
+    tokens = nltk.word_tokenize(doc)                                                          # tokenize document
+    filtered_tokens = [token for token in tokens if token not in stop_words]                  # filter stopwords out of document
+    doc = ' '.join(filtered_tokens)                                                           # re-create document from filtered tokens
+    return doc
+
+normalize_corpus = np.vectorize(normalize_document)
+
+norm_corpus = normalize_corpus(list(combined_df['Description']))
+
+tv = TfidfVectorizer(use_idf=True, min_df=3, max_df=0.8, ngram_range=(1,2), sublinear_tf=True)
+tv_matrix = tv.fit_transform(norm_corpus)
+
+                         # set parameters for tf-idf for unigrams and bigrams
+tfidf_matrix = tv.fit_transform(norm_corpus)                                      # extract tfidf features from norm_corpus
+
+doc_sim = cosine_similarity(tfidf_matrix)    # compute document similarity by examining the cosine similairty b/w documents in matrix
+doc_sim_df = pd.DataFrame(doc_sim)                                                  # take doc_sim, convert to dataframe
+doc_sim_df.head()
+
+# saving all the unique movie titles to a list
+repository_list = combined_df['Repository Name'].values
+
+# Create a Streamlit app
+st.title('Repository Recommender System')
+
+# Input field for search query
+search_query = st.text_input('Enter a search query:')
+
+# Function to find similar repositories based on the search query
+def query_repository_recommender(search_query, repository_list, tfidf_matrix, tv, combined_df):
+    try:
+        # Transform the search query into its vector form
+        query_vector = tv.transform([search_query])
+
+        cosine_similarities = cosine_similarity(query_vector, tfidf_matrix)
+
+        similar_repository_idxs = cosine_similarities[0].argsort()[-5:][::-1]
+
+        similar_repositories = repository_list[similar_repository_idxs]
+
+        return similar_repositories
+    except Exception as e:
+        return ["Error: " + str(e)]
+
+# Button to trigger the search and display recommendations
+if st.button('Find Similar Repositories'):
+    query_recommendations = query_repository_recommender(search_query, repository_list, tfidf_matrix, tv, combined_df)
+    
+    if "Error" in query_recommendations[0]:
+        st.write("An error occurred:", query_recommendations[0])
+    else:
+        st.write("Based on your search query, I'd recommend checking out:")
+        for repo in query_recommendations:
+            # Retrieve the corresponding repository URL from the DataFrame
+            repo_url = combined_df.loc[combined_df['Repository Name'] == repo, 'Repository URL'].values[0]
+            
+            # Display the repository name as a clickable hyperlink
+            st.markdown(f"[{repo}]({repo_url})")
+
+
+
